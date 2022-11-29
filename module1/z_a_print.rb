5a08ff2266f4b3b067871dbac0b6a51f00c425c7
Pratik Baravkar <pratik.baravkar@joshsoftware.com>
2022-11-29 15:45:53 +0530

    added assignments on inhertance and sql joins

--- module1/z_a_print.rb
+++
@@ -1,8 +0,0 @@
-#Q3. Print a-z in reverse order
-
-myarr = [*?a..?z]       #creating array of a-z alphabets
-iterator = myarr.length
-while iterator >= 0
-  puts  myarr[iterator]  #printng array in reverse order while loop
-  iterator -= 1
-end
