7efbe4467cfe15ec6d2f8d596281dd6656b89de4
Pratik Baravkar <pratik.baravkar@joshsoftware.com>
2022-12-19 14:00:49 +0530

    supplymentary changes

--- module7/Blog/app/controllers/users_controller.rb
+++ module7/Blog/app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      #NewUserEmailMailer.notify_user(user).deliver
       redirect_to '/signup'
       flash[:success] = "Success"
     else
