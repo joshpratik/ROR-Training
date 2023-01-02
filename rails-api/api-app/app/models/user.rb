816c9dde334f2c2b3c9e6c84c26a582beaa87405
Pratik Baravkar <pratik.baravkar@joshsoftware.com>
2023-01-02 14:27:01 +0530

    added suggested changes

--- rails-api/api-app/app/models/user.rb
+++ rails-api/api-app/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
-  def jwt_payload
-    super.merge("foo" => "bar")
-  end
+  # def jwt_payload
+  #   super.merge("foo" => "bar")
+  # end
 end
